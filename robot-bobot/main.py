c4f118311bf9a5b2eb33ecae395a076f28a72c55
Apex-droid <59209023+Apex-droid@users.noreply.github.com>
2024-07-29 23:00:12 +0300

    Update main.py

--- robot-bobot/main.py
+++ robot-bobot/main.py
@@ -11,13 +11,10 @@ from aiogram.filters.command import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram import F
 
-# Включаем логирование, чтобы не пропустить важные сообщения
+
 logging.basicConfig(level=logging.INFO)
 
-# Замените "YOUR_BOT_TOKEN" на токен, который вы получили от BotFather
-API_TOKEN = '6709956091:AAEwUT1pebmSa4GgTti6MccBzMlgsbKqGxM'
 
-# Объект бота
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
